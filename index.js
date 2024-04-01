38c5f34353de9fc46f778724d29cb90db4004ac8
MUSTIADO <farhanmaalim6@gmail.com>
2024-04-02 01:47:17 +0300

    feat:phase-1-first-class-functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(driversArray) {
+    return driversArray.slice(0, 2);
+  };
+  const returnLastTwoDrivers = function(driversArray) {
+    return driversArray.slice(-2);
+  };
+  
+  const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+  ];
+  
+  function createFareMultiplier(multiplier) {
+    return function(value) {
+      return value * multiplier;
+    };
+  }
+  function createFareMultiplier(multiplier) {
+    return function(value) {
+      return value * multiplier;
+    };
+  }
+  
+  const fareDoubler = createFareMultiplier(2);
+
+
+  function createFareMultiplier(multiplier) {
+    return function(value) {
+      return value * multiplier;
+    };
+  }
+  
+  const fareTripler = createFareMultiplier(3);
+
+
+  function selectDifferentDrivers(arrayOfDrivers, selectFunction) {
+    return selectFunction(arrayOfDrivers);
+  }
+  
+  console.log(returnFirstTwoDrivers(drivers)); // Output: ['Antonia', 'Nuru']
+  
+  console.log(returnLastTwoDrivers(drivers)); // Output: ['Amari', 'Mo']
+  
